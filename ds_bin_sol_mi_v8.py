cf0bef9dd81735230ee3a98c49f3b9dff4bd131f
veuve <75466141@qq.com>
2025-12-15 18:05:43 +0800

    1

--- ds_bin_sol_mi_v8.py
+++ ds_bin_sol_mi_v8.py
@@ -33,7 +33,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler(f'trading_bot_{datetime.now().strftime("%Y%m%d")}.log'),
+        logging.FileHandler(f'logs/trading_bot_{datetime.now().strftime("%Y%m%d")}.log'),
         logging.StreamHandler()
     ]
 )
